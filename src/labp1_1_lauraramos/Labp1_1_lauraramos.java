ace9da9d372432ff8815e86a12023a10c136243f
Laura <laura_varela@unitec.edu>
2025-04-25 15:05:36 -0600

    Commit 2 y final

--- src/labp1_1_lauraramos/Labp1_1_lauraramos.java
+++ src/labp1_1_lauraramos/Labp1_1_lauraramos.java
@@ -4,6 +4,8 @@
  */
 package labp1_1_lauraramos;
 
+import java.util.Scanner;
+
 /**
  *
  * @author laura
@@ -14,7 +16,45 @@ public class Labp1_1_lauraramos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        int respuesta = -1;
+        Scanner read = new Scanner(System.in);
+        while (respuesta !=0) {
+        
+            System.out.println("Menu Principal");
+            System.out.println("Opcion 1. Mensaje");
+            System.out.println("Opcion 2. Calculadora");
+            System.out.println("Opcion 3. Edades");
+            
+            respuesta = read.nextInt();
+            
+            
+            if (respuesta == 1) {
+                System.out.println("ur sus");
+                
+            } else if(respuesta == 2) {
+                System.out.println("Ingrese el numero uno: ");
+                int num1 = read.nextInt();
+                System.out.println("Ingrese el segundo numero: ");
+                int num2 = read.nextInt();
+                
+                int resultado = num1 * num2;
+                
+                System.out.println("Resultado: "+resultado);                     
+            } 
+            else if(respuesta == 3) {
+                System.out.println("Ingrese su edad: ");
+                int edad = read.nextInt();
+                
+                if (edad >= 18){
+                    System.out.println("Usted es mayor de edad.");
+               
+                } else {
+                    System.out.println("Usted no es mayor de edad.");
+                }
+                
+            }
+    }
     }
     
 }
