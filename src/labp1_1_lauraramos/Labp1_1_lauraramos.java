01489ba0cfef5d2e3c6a603f7f339d5f8170a1cc
Laura <laura_varela@unitec.edu>
2025-04-25 14:02:41 -0600

    Commit inicial

---
+++ src/labp1_1_lauraramos/Labp1_1_lauraramos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labp1_1_lauraramos;
+
+/**
+ *
+ * @author laura
+ */
+public class Labp1_1_lauraramos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
